8308e1ef4d9aae9ceea49a182970a47a6b0f84b3
Elda-Dani <eldadani2001@gmail.com>
2022-10-10 21:22:25 +0530

    about

---
+++ src/app/gents/gents.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gents',
+  templateUrl: './gents.component.html',
+  styleUrls: ['./gents.component.css']
+})
+export class GentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
